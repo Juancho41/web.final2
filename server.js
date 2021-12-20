ca2cf83d45a98f9e066bfeb6b0d2a0e59cba14a6
JuanC <juan_corica@hotmail.com>
2021-12-20 20:10:33 -0300

    segundo commit

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(cors()); 
 
-app.use('/', comentariosRouter);
+app.use('/comen1', comentariosRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get("/*", (req, res) => 
-    {res.sendFile(path.join(__dirname, "build", "index.html")):
+    {res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 
